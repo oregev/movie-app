7d7af7c2634e8dd63c735e21a7c0f199f215f287
Or <60711866+oregev@users.noreply.github.com>
2023-11-22 19:06:23 +0200

    add 404

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  appType: 'spa',
+  appType: 'mpa',
   base: '/movie-app/',
 });
