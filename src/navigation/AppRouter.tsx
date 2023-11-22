52d741ddfc1025db38b417b1e388840bf5ad0267
Or <60711866+oregev@users.noreply.github.com>
2023-11-22 18:43:23 +0200

    update files

--- src/navigation/AppRouter.tsx
+++ src/navigation/AppRouter.tsx
@@ -1,16 +1,16 @@
-import { Routes, Route, HashRouter, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, BrowserRouter } from 'react-router-dom';
 import { PageLayout } from '../layout';
 import { Favorites, Movie, NotFound } from '../pages';
 
 export const AppRouter = (): JSX.Element => (
-  <HashRouter basename={import.meta.env.BASE_URL}>
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
     <Routes>
       <Route path="/" element={<PageLayout />}>
-        <Route index element={<Navigate to="/home" />} />
-        <Route path="home" element={<Movie />} />
+        <Route index element={<Navigate to="/movie" replace />} />
+        <Route path="movie" element={<Movie />} />
         <Route path="favorites" element={<Favorites />} />
       </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
-  </HashRouter>
+  </BrowserRouter>
 );
